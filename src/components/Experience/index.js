fa84c8fa03474b0c8c3078b0e3ed8b3a4a9019e2
Bruno Vieira Carlini Charamba <brunocharamba@gmail.com>
2020-06-10 15:44:51 -0300

    correction

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import {
   Container,
@@ -10,69 +10,71 @@ import {
   Location,
   Description,
   Date,
-  Separator
-} from './styles'
+  Separator,
+} from "./styles";
 
 export default class Experience extends Component {
   state = {
     experiences: [
-      title: 'software analyst',
-        location: '@joanajuliao',
-        locationUrl: 'https://www.instagram.com/joanajuliaomarca/',
-        description: 'Frontend and Backend developer with MySQL/Mongo databases, React and Node.',
-        date: '2019-2020'
-      },
       {
-        title: 'sr. software developer',
-        location: '@avanade',
-        locationUrl: 'https://www.avanade.com/',
+        title: "software analyst",
+        location: "@joanajuliao",
+        locationUrl: "https://www.instagram.com/joanajuliaomarca/",
         description:
-          'Frontend, Sharepoint and Xamarin developer with C# backend and SQLServer database',
-        date: '2019-2019'
+          "Frontend and Backend developer with MySQL/Mongo databases, React and Node.",
+        date: "2019-2020",
       },
       {
-        title: 'software developer',
-        location: '@avanade',
-        locationUrl: 'https://www.avanade.com/',
+        title: "sr. software developer",
+        location: "@avanade",
+        locationUrl: "https://www.avanade.com/",
         description:
-          'Frontend, Sharepoint and Xamarin developer with C# backend and SQLServer database',
-        date: '2018-2019'
+          "Frontend, Sharepoint and Xamarin developer with C# backend and SQLServer database",
+        date: "2019-2019",
       },
       {
-        title: 'software analyst',
-        location: '@joanajuliao',
-        locationUrl: 'https://www.instagram.com/joanajuliaomarca/',
-        description: 'Frontend and PHP developer with MySQL database',
-        date: '2016-2018'
-      },
-      {
-        title: 'jr. software developer',
-        location: '@allen',
-        locationUrl: 'http://allen.com.br/',
+        title: "software developer",
+        location: "@avanade",
+        locationUrl: "https://www.avanade.com/",
         description:
-          'Frontend and Sharepoint developer with C# backend and SQLServer database',
-        date: '2014-2016'
-      }
+          "Frontend, Sharepoint and Xamarin developer with C# backend and SQLServer database",
+        date: "2018-2019",
+      },
+      {
+        title: "software analyst",
+        location: "@joanajuliao",
+        locationUrl: "https://www.instagram.com/joanajuliaomarca/",
+        description: "Frontend and PHP developer with MySQL database",
+        date: "2016-2018",
+      },
+      {
+        title: "jr. software developer",
+        location: "@allen",
+        locationUrl: "http://allen.com.br/",
+        description:
+          "Frontend and Sharepoint developer with C# backend and SQLServer database",
+        date: "2014-2016",
+      },
     ],
     education: [
       {
-        title: 'bootcamp rocketseat',
-        location: '@rocketseat',
-        locationUrl: 'https://rocketseat.com.br/',
+        title: "bootcamp rocketseat",
+        location: "@rocketseat",
+        locationUrl: "https://rocketseat.com.br/",
         description:
-          'An intensive bootcamp focused in react, react native and nodejs. ',
-        date: '2019-2019'
+          "An intensive bootcamp focused in react, react native and nodejs. ",
+        date: "2019-2019",
       },
       {
-        title: 'bsc. computer engineering',
-        location: '@ecomp-poli-upe',
-        locationUrl: 'http://upe.poli.br/',
+        title: "bsc. computer engineering",
+        location: "@ecomp-poli-upe",
+        locationUrl: "http://upe.poli.br/",
         description:
           "Bachelor's degree in computer engineering attend in Universidade de Pernambuco",
-        date: '2008-2014'
-      }
-    ]
-  }
+        date: "2008-2014",
+      },
+    ],
+  };
 
   render() {
     return (
@@ -81,19 +83,19 @@ export default class Experience extends Component {
           <Wrapper>
             <Title>education</Title>
             <Separator>.</Separator>
-            {this.state.education.map(edu => {
+            {this.state.education.map((edu) => {
               return (
                 <Item key={edu.title + Math.random()}>
                   <ItemTitle>{edu.title}</ItemTitle>
                   <Location
-                    onClick={() => window.open(edu.locationUrl, '_blank')}
+                    onClick={() => window.open(edu.locationUrl, "_blank")}
                   >
                     {edu.location}
                   </Location>
                   <Description>{edu.description}</Description>
                   <Date>{edu.date}</Date>
                 </Item>
-              )
+              );
             })}
           </Wrapper>
         </Column>
@@ -101,12 +103,12 @@ export default class Experience extends Component {
           <Wrapper>
             <Title>experience</Title>
             <Separator>.</Separator>
-            {this.state.experiences.map(exp => {
+            {this.state.experiences.map((exp) => {
               return (
                 <Item key={exp.title + Math.random()}>
                   <ItemTitle>{exp.title}</ItemTitle>
                   <Location
-                    onClick={() => window.open(exp.locationUrl, '_blank')}
+                    onClick={() => window.open(exp.locationUrl, "_blank")}
                   >
                     {exp.location}
                   </Location>
@@ -115,11 +117,11 @@ export default class Experience extends Component {
                   </Description>
                   <Date>{exp.date}</Date>
                 </Item>
-              )
+              );
             })}
           </Wrapper>
         </Column>
       </Container>
-    )
+    );
   }
 }
